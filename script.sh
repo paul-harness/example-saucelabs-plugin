75954c76bf32555bce208d20cd22bb4f464da818
paul-harness <56152762+paul-harness@users.noreply.github.com>
2022-12-08 05:33:05 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,3 @@
-echo ${PLUGIN_MESSAGE}
-echo "curling" ${PLUGIN_URL}
-curl ${PLUGIN_URL}
+saucectl configure -u ${PLUGIN_USERNAME} -a ${PLUGIN_KEY}
+saucectl init -u ${PLUGIN_USERNAME} -a ${PLUGIN_KEY} -r ${PLUGIN_REGION} -f cypress -v 7.3.0 --cypress.config cypress.json -b ${PLUGIN_BROWSER} -p "windows 10" --artifacts.download.when always
 echo "plugin executed successfully"
